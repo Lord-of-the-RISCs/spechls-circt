0dcc3d5777afc2022af26f73c6edde14ea4ebeb5
Jean-Michel Gorius <jean-michel.gorius@irisa.fr>
2025-05-19 17:07:21 +0200

    Add spechls-reduce

---
+++ tools/spechls-reduce/spechls-reduce.cpp
@@ -0,0 +1,21 @@
+//
+// This file is part of the SpecHLS project.
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#include <circt/Dialect/SSP/SSPDialect.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/Tools/mlir-reduce/MlirReduceMain.h>
+
+#include "Dialect/Schedule/IR/Schedule.h"
+#include "Dialect/SpecHLS/IR/SpecHLS.h"
+
+int main(int argc, char **argv) {
+  mlir::DialectRegistry registry;
+  registry.insert<spechls::SpecHLSDialect, schedule::ScheduleDialect, circt::ssp::SSPDialect>();
+  mlir::MLIRContext context(registry);
+
+  return mlir::failed(mlir::mlirReduceMain(argc, argv, context));
+}
