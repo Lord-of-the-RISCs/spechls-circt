0d524fc54a86b768cb50bf6b846da458706c9fc7
Dylan Leothaud <dylan.leothaud@irisa.fr>
2025-05-21 11:09:05 +0200

    create conversion from schedule to ssp

---
+++ include/Conversion/Passes.h
@@ -0,0 +1,23 @@
+//
+// This file is part of the SpecHLS project.
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#ifndef SPECHLS_CONVERSION_PASSES_H
+#define SPECHLS_CONVERSION_PASSES_H
+
+#include "circt/Support/LLVM.h"
+#include <memory>
+
+namespace spechls {
+  
+std::unique_ptr<mlir::Pass> createScheduleToSSP();
+
+#define GEN_PASS_REGISTRATION
+#include "Conversion/Passes.h.inc"
+
+}; // namespace spechls
+
+#endif // SPECHLS_CONVERSION_PASSES_H
