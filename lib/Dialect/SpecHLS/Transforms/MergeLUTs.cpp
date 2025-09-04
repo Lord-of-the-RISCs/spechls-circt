7445c4396c5320485e1561de19dac01cd2dfc3fc
Jean-Michel Gorius <jean-michel.gorius@irisa.fr>
2025-09-04 11:15:21 +0200

    Add initial MergeLUTs implementation

---
+++ lib/Dialect/SpecHLS/Transforms/MergeLUTs.cpp
@@ -0,0 +1,77 @@
+//
+// This file is part of the SpecHLS project.
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#include "Dialect/SpecHLS/Transforms/Passes.h" // IWYU pragma: keep
+
+#include <circt/Dialect/Comb/CombOps.h>
+#include <circt/Dialect/HW/HWOps.h>
+#include <mlir/Dialect/PDL/IR/PDL.h>
+#include <mlir/Dialect/PDLInterp/IR/PDLInterp.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/PatternMatch.h>
+#include <mlir/IR/Value.h>
+#include <mlir/IR/ValueRange.h>
+#include <mlir/Parser/Parser.h>
+#include <mlir/Pass/Pass.h>
+#include <mlir/Pass/PassManager.h>
+#include <mlir/Support/TypeID.h>
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+
+#include "Dialect/SpecHLS/IR/SpecHLSOps.h"
+#include "Utils.h"
+#include "mlir/Support/LLVM.h"
+
+using namespace mlir;
+
+namespace spechls {
+#define GEN_PASS_DEF_MERGELUTSPASS
+#include "Dialect/SpecHLS/Transforms/Passes.h.inc"
+} // namespace spechls
+
+#include "MergeLUTs.h.inc"
+
+namespace {
+
+struct MergeLUTsPass : public spechls::impl::MergeLUTsPassBase<MergeLUTsPass> {
+  FrozenRewritePatternSet patterns;
+
+  using MergeLUTsPassBase::MergeLUTsPassBase;
+
+  LogicalResult initialize(MLIRContext *ctx) override {
+    // Building the pattern set inside of the `initialize` method pre-compiles the patterns into bytecode. If we don't
+    // provide this function, patterns would be recompiled for each `runOnOperation` invocation.
+    RewritePatternSet patternList{ctx};
+    registerNativeRewrite(patternList);
+    populateGeneratedPDLLPatterns(patternList);
+    patterns = std::move(patternList);
+    return success();
+  }
+
+  void registerNativeRewrite(RewritePatternSet &patterns) {
+    patterns.getPDLPatterns().registerRewriteFunction("CollapseLUTs", collapseLUTsImpl);
+  }
+
+  void runOnOperation() override { (void)applyPatternsGreedily(getOperation(), patterns); }
+
+private:
+  static Operation *collapseLUTsImpl(PatternRewriter &rewriter, Operation *op) {
+    auto root = cast<spechls::LUTOp>(op);
+    auto arg = cast<spechls::LUTOp>(root.getIndex().getDefiningOp());
+
+    ArrayRef<int64_t> rootContents = root.getContents();
+    ArrayRef<int64_t> argContents = arg.getContents();
+
+    SmallVector<int64_t> collapsedContents;
+    for (size_t i = 0; i < argContents.size(); ++i) {
+      collapsedContents.push_back(rootContents[argContents[i]]);
+    }
+
+    return rewriter.create<spechls::LUTOp>(root.getLoc(), root.getType(), arg.getIndex(), collapsedContents);
+  }
+};
+
+} // namespace
---
+++ test/SpecHLS/Transforms/merge-luts.mlir
@@ -0,0 +1,10 @@
+// RUN: spechls-opt -split-input-file --merge-luts %s | spechls-opt | FileCheck %s
+
+// CHECK-LABEL: @consecutive
+spechls.kernel @consecutive(%x: i32) -> i3 {
+  %true = hw.constant 1 : i1
+  %0 = spechls.lut %x [1, 3, 2, 1] : (i32) -> i2
+  // CHECK: %{{.*}} = spechls.lut %arg0 [2, 6, 4, 2] : (i32) -> i3
+  %1 = spechls.lut %0 [0, 2, 4, 6] : (i2) -> i3
+  spechls.exit if %true with %1 : i3
+}
