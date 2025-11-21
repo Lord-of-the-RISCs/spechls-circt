6e37162c29d58456f409b563d3239ca129247329
Thomas Feuilletin <thomas.feuilletin@inria.fr>
2025-11-21 09:18:41 +0100

    Add outline-core pass

---
+++ lib/Dialect/Wcet/Transforms/OutlineCore.cpp
@@ -0,0 +1,173 @@
+//===- OutlineCore.cpp  ---------*- C++ -*-===//
+//===----------------------------------------------------------------------===//
+//
+// Contains the definition of the OutlineCore pass
+//
+//===----------------------------------------------------------------------===//
+
+#include "Dialect/SpecHLS/IR/SpecHLSTypes.h"
+#include "circt/Dialect/HW/HWOps.h"
+#include "circt/Support/LLVM.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/IRMapping.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Transforms/InliningUtils.h"
+#include "mlir/Transforms/Passes.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/DebugLog.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <Dialect/SpecHLS/IR/SpecHLSOps.h>
+#include <Dialect/Wcet/IR/WcetOps.h>
+#include <string>
+
+#define DEBUG_TYPE "OutlineCore"
+
+using namespace mlir;
+
+namespace wcet {
+#define GEN_PASS_DEF_OUTLINECOREPASS
+#include "Dialect/Wcet/Transforms/Passes.h.inc"
+} // namespace wcet
+
+namespace wcet {
+
+struct OutlineCorePass : public impl::OutlineCorePassBase<OutlineCorePass> {
+
+  using OutlineCorePassBase::OutlineCorePassBase;
+
+public:
+  void runOnOperation() override {
+    auto mod = getOperation();
+
+    /**************************************************************************
+     * Get the speculative Task                                               *
+     *************************************************************************/
+    spechls::TaskOp speculativeTask = nullptr;
+    mod->walk([&](spechls::TaskOp t) {
+      if (t->getAttr("spechls.speculative")) {
+        speculativeTask = t;
+        return;
+      }
+    });
+
+    if (!speculativeTask) {
+      LLVM_DEBUG({ LDBG() << "No speculative spechls.task found"; });
+      signalPassFailure();
+    }
+
+    /**************************************************************************
+     * Retrieve core input's types, fetch and attributes                      *
+     *************************************************************************/
+    bool isResultsPacked = !speculativeTask.getOps<spechls::PackOp>().empty();
+
+    OpBuilder builder = OpBuilder(&getContext());
+    SmallVector<Type> funInputsTypes = SmallVector<Type>(speculativeTask.getOperandTypes());
+    SmallVector<Type> funOutputsTypes;
+    SmallVector<std::string> funOutputsNames;
+    SmallVector<DictionaryAttr> inputsAttrs;
+    for (size_t i = 0; i < funInputsTypes.size(); ++i) {
+      inputsAttrs.push_back(DictionaryAttr());
+    }
+    bool fetchFound = false;
+    int delCount = 0;
+    int muCount = 0;
+    int packCount = 0;
+    speculativeTask->walk([&](Operation *op) {
+      if (op->getAttr("wcet.fetch")) {
+        op->setAttr("wcet.toReplace", builder.getI64IntegerAttr(funInputsTypes.size()));
+        funInputsTypes.push_back(op->getResultTypes().front());
+        inputsAttrs.push_back(builder.getDictionaryAttr(builder.getNamedAttr("wcet.instrs", builder.getUnitAttr())));
+        fetchFound = true;
+        return;
+      }
+      auto opName = op->getName().getStringRef();
+      if (opName == spechls::DelayOp::getOperationName().str()) {
+        auto delay = cast<spechls::DelayOp>(op);
+        inputsAttrs.push_back(DictionaryAttr());
+        op->setAttr("wcet.toReplace", builder.getI64IntegerAttr(funInputsTypes.size()));
+        delay.getInput().getDefiningOp()->setAttr("wcet.output", builder.getUnitAttr());
+        funOutputsTypes.push_back(delay.getInput().getType());
+        funOutputsNames.push_back("delay_" + std::to_string(delCount++));
+        funInputsTypes.push_back(op->getResultTypes().front());
+      }
+      if (opName == spechls::MuOp::getOperationName().str()) {
+        auto mu = cast<spechls::MuOp>(op);
+        inputsAttrs.push_back(DictionaryAttr());
+        op->setAttr("wcet.toReplace", builder.getI64IntegerAttr(funInputsTypes.size()));
+        mu.getLoopValue().getDefiningOp()->setAttr("wcet.output", builder.getUnitAttr());
+        funOutputsTypes.push_back(mu.getType());
+        funOutputsNames.push_back("mu_" + std::to_string(muCount++));
+        funInputsTypes.push_back(op->getResultTypes().front());
+      }
+      if (opName == spechls::PackOp::getOperationName().str()) {
+        auto pack = cast<spechls::PackOp>(op);
+        for (auto v : pack.getInputs()) {
+          v.getDefiningOp()->setAttr("wcet.output", builder.getUnitAttr());
+          funOutputsNames.push_back("pack_" + std::to_string(packCount++));
+          funOutputsTypes.push_back(v.getType());
+        }
+      }
+      if (opName == spechls::CommitOp::getOperationName().str() && !isResultsPacked) {
+        auto commit = cast<spechls::CommitOp>(op);
+        funOutputsNames.push_back("result");
+        funOutputsTypes.push_back(commit.getValue().getType());
+        commit.getValue().getDefiningOp()->setAttr("wcet.output", builder.getUnitAttr());
+      }
+    });
+
+    if (!fetchFound) {
+      LLVM_DEBUG({ LDBG() << "No fetch found"; });
+      signalPassFailure();
+    }
+
+    /**************************************************************************
+     * Create the core Operation                                              *
+     *************************************************************************/
+    builder.setInsertionPointToStart(mod.getBody());
+    auto funType = builder.getFunctionType(
+        funInputsTypes, builder.getType<spechls::StructType>("out", funOutputsNames, funOutputsTypes));
+    auto core =
+        builder.create<wcet::CoreOp>(builder.getUnknownLoc(), funType,
+                                     builder.getStringAttr(std::string("core_" + speculativeTask.getSymName().str())),
+                                     ArrayRef<DictionaryAttr>(inputsAttrs));
+
+    builder.setInsertionPointToStart(&core.getBody().getBlocks().front());
+    IRMapping mapper;
+    for (const auto &it : llvm::enumerate(speculativeTask.getBody().getArguments())) {
+      mapper.map(it.value(), core.getBody().getArgument(it.index()));
+    }
+
+    SmallVector<Value> outputsValue;
+    speculativeTask->walk([&](Operation *op) {
+      auto opName = op->getName().getStringRef();
+      if (opName == spechls::TaskOp::getOperationName().str() ||
+          opName == spechls::CommitOp::getOperationName().str() ||
+          opName == spechls::PackOp::getOperationName().str()) {
+        return;
+      }
+      auto *newOp = builder.clone(*op, mapper);
+      if (newOp->getAttr("wcet.output")) {
+        outputsValue.push_back(newOp->getResult(0));
+      }
+    });
+
+    auto pack = builder.create<spechls::PackOp>(builder.getUnknownLoc(), funType.getResults(), outputsValue);
+    builder.create<wcet::CommitOp>(builder.getUnknownLoc(), pack);
+
+    core->walk([&](Operation *op) {
+      auto attr = dyn_cast_or_null<IntegerAttr>(op->getAttr("wcet.toReplace"));
+      if (attr) {
+        int64_t idx = attr.getInt();
+        op->getResult(0).replaceAllUsesWith(core.getBody().getArgument(idx));
+        op->erase();
+      }
+    });
+  }
+};
+
+} // namespace wcet
---
+++ test/Wcet/Transforms/outline-core.mlir
@@ -0,0 +1,16 @@
+// RUN: spechls-opt -split-input-file --outline-core %s | spechls-opt |FileCheck %s
+
+//CHECK: wcet.core @core_test(%arg0: i32, %arg1: i1, %arg2: !spechls.array<i32, 32>, %arg3: i32 {wcet.instrs}, %arg4: i32) -> !spechls.struct<"out" { "delay_0" : i32, "result" : i32 }>
+//CHECK-NOT: spechls.delay
+//CHECK: wcet.commit
+
+spechls.kernel @simpl(%in0 : i32, %in1 : !spechls.array<i32, 32>) -> i32 {
+    %true = hw.constant 1 : i1
+    %result = spechls.task "test" (%0 = %in0, %1 = %true, %arr = %in1) : (i32, i1, !spechls.array<i32, 32>)  -> i32 attributes {spechls.speculative} {
+        %2 = comb.add %0, %0 : i32
+        %3 = spechls.load %arr[%2 : i32] {wcet.fetch} : <i32, 32>
+        %4 = spechls.delay %3 by 1 {spechls.test} : i32
+        spechls.commit %1, %4 : i32
+      }
+      spechls.exit if %true with %result : i32 
+  }
