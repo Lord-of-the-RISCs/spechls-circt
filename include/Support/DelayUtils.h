05e4e19c73e4d942ff27f00bc80360234c96e063
Dylan Leothaud <dylan.leothaud@irisa.fr>
2025-12-04 07:50:25 +0100

    add walkOnDelay on block pointer, and merge declaration and definition as a templated function

--- include/Support/DelayUtils.h
+++ include/Support/DelayUtils.h
@@ -1,9 +1,25 @@
 #ifndef SPECHLS_DELAY_UTILS_H
 #define SPECHLS_DELAY_UTILS_H
 
+#include "Dialect/SpecHLS/IR/SpecHLSOps.h"
+
+#include <llvm/ADT/TypeSwitch.h>
 #include <mlir/IR/Dialect.h>
 
 template <typename T, typename F>
-void walkOnDelay(T &&block, F &&fun);
+void walkOnDelay(T &&block, F &&fun) {
+  block.walk([&](mlir::Operation *op) {
+    llvm::TypeSwitch<mlir::Operation *, void>(op)
+        .Case<spechls::DelayOp, spechls::CancellableDelayOp, spechls::RollbackableDelayOp>(fun);
+  });
+}
+
+template <typename T, typename F>
+void walkOnDelay(T *block, F &&fun) {
+  block->walk([&](mlir::Operation *op) {
+    llvm::TypeSwitch<mlir::Operation *, void>(op)
+        .Case<spechls::DelayOp, spechls::CancellableDelayOp, spechls::RollbackableDelayOp>(fun);
+  });
+}
 
 #endif
