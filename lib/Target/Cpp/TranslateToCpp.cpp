83c1844830d4a008104bbb2a3c63fb9b6b8cf9fe
srokicki <simon.rokicki@irisa.fr>
2026-05-27 10:29:46 +0200

    correct various bugs on rollback generation

--- lib/Target/Cpp/TranslateToCpp.cpp
+++ lib/Target/Cpp/TranslateToCpp.cpp
@@ -238,8 +238,13 @@ LogicalResult printAllVariables(CppEmitter &emitter, spechls::KernelOp &kernelOp
     } else if (auto rollbackOp = dyn_cast<spechls::RollbackOp>(op)) {
       if (failed(emitter.emitType(op->getLoc(), rollbackOp.getType())))
         return failure();
-      os << " " << getRollbackBufferName(emitter, rollbackOp) << "["
-         << *std::max_element(rollbackOp.getDepths().begin(), rollbackOp.getDepths().end()) + 1;
+        
+      if (rollbackOp.getDepths().empty())
+        os << " " << getRollbackBufferName(emitter, rollbackOp) << "["
+            << 0;
+      else
+        os << " " << getRollbackBufferName(emitter, rollbackOp) << "["
+            << *std::max_element(rollbackOp.getDepths().begin(), rollbackOp.getDepths().end()) + 1;
       if (emitter.shouldGenerateCatapultCompatibleCode())
         os << "];\n";
       else
@@ -424,14 +429,16 @@ LogicalResult printOperation(CppEmitter &emitter, ModuleOp moduleOp) {
   for (auto &&mop : moduleOp.getBodyRegion().front()) {
     if (auto kernelOp = dyn_cast<spechls::KernelOp>(mop)) {
       for (auto &&op : kernelOp.getBody().front()) {
-        if (auto rollbackOp = dyn_cast<spechls::RollbackOp>(op)) {
-          maxRollback =
-              std::max(maxRollback, *std::max_element(rollbackOp.getDepths().begin(), rollbackOp.getDepths().end()));
+        if (auto rollbackOp = dyn_cast<spechls::RollbackOp>(op)){
+            if (!rollbackOp.getDepths().empty())
+              maxRollback =
+                  std::max(maxRollback, *std::max_element(rollbackOp.getDepths().begin(), rollbackOp.getDepths().end()));
         } else if (auto taskOp = dyn_cast<spechls::TaskOp>(op)) {
           for (auto &&op2 : taskOp.getBody().front()) {
-            if (auto rollbackOp2 = dyn_cast<spechls::RollbackOp>(op2)) {
-              maxRollback = std::max(maxRollback,
-                                     *std::max_element(rollbackOp2.getDepths().begin(), rollbackOp2.getDepths().end()));
+            if (auto rollbackOp2 = dyn_cast<spechls::RollbackOp>(op2)){
+                if (!rollbackOp2.getDepths().empty()) 
+                  maxRollback = std::max(maxRollback,
+                                         *std::max_element(rollbackOp2.getDepths().begin(), rollbackOp2.getDepths().end()));
             }
           }
         }
@@ -979,7 +986,9 @@ LogicalResult printOperation(CppEmitter &emitter, spechls::RollbackOp rollbackOp
     os << "\n{\n";
     os.indent();
 
-    size_t maxDepth = *std::max_element(rollbackOp.getDepths().begin(), rollbackOp.getDepths().end());
+    size_t maxDepth = 0;
+    if (!rollbackOp.getDepths().empty())
+         maxDepth = *std::max_element(rollbackOp.getDepths().begin(), rollbackOp.getDepths().end());
     os << "unsigned int off = ";
     if (failed(emitter.emitOperand(rollbackOp.getRollback())))
       return failure();
